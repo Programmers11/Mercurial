3a316e437d78091876341d67f876bff3d71bf053
Jahanzeb2199 <k112199@nu.edu.pk>
2015-10-31 20:21:46 +0500

    Mercr Report Build+ Revert sql update

--- Database/Revert_GST/mercreports.sql
+++ Database/Revert_GST/mercreports.sql
@@ -1,4 +1,4 @@
-
+drop view mercreports;
 
 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `mercreports` AS select `b`.`rcptNo` AS `Voucher No`,`b`.`phone` AS `Phone`,`c`.`clientName` AS `Customer`,`b`.`status` AS `Status`,`b`.`issueDate` AS `IssueDate`,`b`.`deldate` AS `deldate`,sum((`info`.`qty` * (select `cl`.`countVal` from `clothes` `cl` where (`cl`.`cid` = `info`.`cid`)))) AS `qty`,`b`.`netAmount` AS `netAmount`,`b`.`gst` AS `gst%`,((`b`.`netAmount` * `b`.`gst`) / (100 + `b`.`gst`)) AS `Gst_Amt`,(`b`.`netAmount` - ((`b`.`netAmount` * `b`.`gst`) / (100 + `b`.`gst`))) AS `GrosAmount` from ((`booking` `b` join `booking_clothes` `info`) join `client` `c`) where ((`info`.`rcptNo` = `b`.`rcptNo`) and (`b`.`phone` = `c`.`phone`)) group by `b`.`rcptNo`;
 
